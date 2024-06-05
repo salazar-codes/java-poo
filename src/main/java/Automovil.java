5dc70af0a745fdfd2a55a8242e5041cbb33d958d
Jimmy Salazar <jimmy.salazar@tektonlabs.com>
2024-06-05 00:07:10 -0500

    feat: add classes and objects

---
+++ src/main/java/Automovil.java
@@ -0,0 +1,85 @@
+public class Automovil {
+    private String fabricante;
+    private String modelo;
+    private String color = "black";
+    private double cilindrada;
+    private int capacidadTanque = 40;
+
+    public Automovil() {
+    }
+
+    public Automovil(String fabricante, String modelo) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadTanque() {
+        return capacidadTanque;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    public void detalle(){
+        // this: para hacer referencia de la misma clase. También se puede omitir, pero no siempre. Mejor ponerlo.
+        // por ejemplo si tengo una variable dentro -> Stirng modelo = "";
+        System.out.println("---- Detalle del automovil ----");
+        System.out.println("this.fabricante = " + this.fabricante);
+        System.out.println("this.modelo = " + this.modelo);
+        System.out.println("this.color = " + this.color);
+
+        // Buenas prácticas: Dentro de una clase no es buena práctica imprimir datos en consola, mejor devolverlos resumidos.
+        // Esto acopla a una sola forma de imprimir datos
+    }
+
+    public String detalleStr(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("---- Detalle del automovil devolviendo un string ----");
+        sb.append("\nthis.fabricante = " + getFabricante());
+        sb.append("\nthis.modelo = " + this.modelo);
+        sb.append("\nthis.color = " + getColor());
+
+        return sb.toString();
+    }
+
+    // Sobrecarga de métodos: Mismo nombre de método pero distintos tipos o cantidad de parámetros
+    public float calcularConsumo(int km, float porcentajeGasolina){
+      return km/(capacidadTanque * porcentajeGasolina);
+    };
+    public float calcularConsumo(int km, int porcentajeGasolina){
+        return km/(capacidadTanque * (porcentajeGasolina/100f));
+    };
+}
---
+++ src/main/java/EjemploAutomovil.java
@@ -0,0 +1,22 @@
+public class EjemploAutomovil {
+    public static void main(String[] args) {
+
+        Automovil automovil = new Automovil(); // Crea la instancia, la inicializa y se guarda la referencia en memoria hacia ese objeto.
+
+        // automovil.cilindrada = 1.5;
+        automovil.setCilindrada(1.5);
+        // automovil.fabricante = "Honda";
+        automovil.setFabricante("Honda");
+
+        //System.out.println("automovil = " + automovil.fabricante); // Por ahora accediendo a los atributos de forma pública
+        //System.out.println("automovil = " + automovil.color);
+        //System.out.println("automovil = " + automovil.cilindrada);
+
+        //automovil.detalle();
+        String strDetalle = automovil.detalleStr();
+        System.out.println(strDetalle);
+
+        System.out.println("Kilómetros por litro = " + automovil.calcularConsumo(300, 0.6f));
+        System.out.println("Kilómetros por litro = " + automovil.calcularConsumo(300, 60));
+    }
+}
